a2a097bb555913173a0d126ef52ed12c97b29fa6
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-03-30 13:24:24 +0200

    feat: simplify repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      customCss: ["./src/styles/global.css"],
-      title: "My Docs",
-    }),
-  ],
+  integrations: [mdx()],
 });
